2cea06752fa00ac470df32ca90c5d70ed758b1b1
Edouard Tourail <edouard.tourail@gmail.com>
2017-06-16 18:52:48 +0200

    remove docker from versionning

--- docker/rabbit/rabbit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-up=`sv status rabbit`
-if ! [[ "${up}" =~ ^(run: rabbit) ]]; then
-    rabbitmqctl add_user user password
-	rabbitmqctl add_vhost vhost
-	rabbitmqctl set_permissions -p vhost user ".*" ".*" ".*"
-fi
-
-
