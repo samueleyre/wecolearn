0ed0811922ef07899f82c8b6253d6b767919c6cb
Édouard Touraille <edouard.touraille@gmail.com>
2017-12-02 13:25:23 +0100

    [Add unecessary command]

---
+++ app/src/WcBundle/Command/ClientTag.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace WcBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class ClientTag extends Command
+{
+
+    public function __construct( $em )
+    {
+
+        $this->em = $em;
+        parent::__construct();
+
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('app:ct')
+            ->setDescription('Set user_tag');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        $query = " SELECT * FROM client_tag WHERE tag_id != 20 GROUP BY client_id ";
+
+        $stmp = $this->em->getConnection()->prepare( $query );
+        $stmp->execute();
+        $rows = $stmp->fetchAll();
+
+        foreach( $rows as $row ) {
+          $query = sprintf(
+            "UPDATE client_tag
+            SET tag_id = 20
+            WHERE client_id = %d
+            AND tag_id = %d", $row['client_id'], $row['tag_id']);
+
+          $stmp = $this->em->getConnection()->prepare( $query );
+          $stmp->execute();
+
+        }
+
+      }
+}
