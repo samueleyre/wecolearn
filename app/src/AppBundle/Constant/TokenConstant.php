9771334cf0b714d6a61af833df4bd5ebf7069120
samueleyre <samuel.eyre@hotmail.fr>
2018-07-31 20:03:20 +0200

    official V1 package

---
+++ app/src/AppBundle/Constant/TokenConstant.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: sam
+ * Date: 31/07/18
+ * Time: 12:17
+ */
+
+
+class tokenConstant {
+
+
+  public static types = [
+    "CONFIRMEMAIL" => 0,
+
+  ]
+
+
+}
