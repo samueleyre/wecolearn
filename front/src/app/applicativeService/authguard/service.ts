cb30fed1a3e6930cbb03a6e56fd4c1859587fa3d
samueleyre <samuel.eyre@hotmail.fr>
2018-11-07 12:27:06 +0100

    removed http that wasn't httpclient

--- front/src/app/applicativeService/authguard/service.ts
+++ front/src/app/applicativeService/authguard/service.ts
@@ -1,6 +1,5 @@
 import { Injectable }                       from '@angular/core';
 import { Router, CanActivate , ActivatedRouteSnapshot, RouterStateSnapshot}              from '@angular/router';
-import { Http }                             from '@angular/http';
 import { TokenService }                     from './../token/service';
 import { Logged }                           from './logged';
 import { Observable }                       from "rxjs/Rx";
@@ -9,7 +8,7 @@ import { PingService }                             from  './../ping/service';
 @Injectable()
 export class AuthGuard implements CanActivate {
  
-    constructor(private router: Router, private tokenService: TokenService, private http: Http, private pingService: PingService ) { }
+    constructor(private router: Router, private tokenService: TokenService, private pingService: PingService ) { }
 
     canActivate ( route: ActivatedRouteSnapshot,state: RouterStateSnapshot): Observable<boolean>|Promise<boolean>|boolean {
       return this.pingService.ping(state.url);
