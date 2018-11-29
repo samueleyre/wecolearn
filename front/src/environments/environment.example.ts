65bf62109b8dfea8e9a98ec5c2cfccc9aefd9a9e
samueleyre <samuel.eyre@hotmail.fr>
2018-11-29 12:26:53 +0100

    added exemple environments for front

---
+++ front/src/environments/environment.example.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  origin : "https://api.domain.com",
+  publique : "https://api.domain.com",
+  mapApiKey :  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  production : true
+};
+
+// needs environment.prod.ts & environment.ts see angular.json
