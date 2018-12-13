581f69a8fea7d9be392d06491bcdb5e1cc9eef56
samueleyre <samuel.eyre@hotmail.fr>
2018-12-13 20:34:03 +0100

    updated package.json for scalingo

--- front/src/app/app.component.spec.ts
+++ front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'newfront'`, () => {
+  it(`should have as title 'front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('newfront');
+    expect(app.title).toEqual('front');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to newfront!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to front!');
   });
 });
