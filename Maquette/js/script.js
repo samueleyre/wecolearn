62111c40f64cf67bef3eac67034de7aeac039c51
Alexandre Moreira <alexandre.moreira.pro@gmail.com>
2019-02-02 16:43:21 +0100

    add maquette folder

---
+++ Maquette/js/script.js
@@ -0,0 +1,46 @@
+window.sr = ScrollReveal();
+sr.reveal('.banner .logo', {
+    duration: 700,
+    distance: '100px',
+    origin: 'left',
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.8,
+
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.about .title', {
+    duration: 700,
+    distance: '300px',
+    origin: 'top',
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.8,
+
+});
+
+window.sr = ScrollReveal();
+sr.reveal('.banner .description', {
+    duration: 700,
+    distance: '200px',
+    origin: 'top',
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.8,
+
+});
+
+
+window.sr = ScrollReveal();
+sr.reveal('.card, .members figure', {
+    interval: 200,
+    duration: 700,
+    distance: '300px',
+    origin: 'right',
+    reset: false,
+    useDelay: 'always',
+    viewFactor: 0.8,
+
+});
+
