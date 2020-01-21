d1b3c1f1a0a906d7d73ad41ff76b96eae31a0223
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-01-21 18:24:00 +0100

    update mercure env

--- front/src/environments/environment.staging.ts
+++ front/src/environments/environment.staging.ts
@@ -3,6 +3,6 @@ export const environment = {
   publique : 'https://api-staging.wecolearn.com',
   mapApiKey : 'AIzaSyA9v3Cne0PPDnAal2Gt1jAbeGmxSym5Ltg',
   production : true,
-  mercureUrl : 'https://mercure.osc-fr1.scalingo.io/hub',
+  mercureUrl : 'https://mercure-staging.wecolearn.com/hub',
   vapidPublic: 'BI4_eiMKp80OeVlgpIlgz219DCMwQqU1ZDwikXdsOnMBozGUFz0Knrm2503Fp4d9jqGWoMmASuGv6w38ABIc6ZY',
 };
