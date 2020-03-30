af2ce0edfa51f4532ab5cf3176ed07647b4b49a3
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-03-30 09:17:53 +0200

    up: ui peer-learning page

--- front/src/app/config/navigation/seo.const.ts
+++ front/src/app/config/navigation/seo.const.ts
@@ -2,12 +2,12 @@ import { NAV } from '~/config/navigation/nav';
 
 export const navTitles = {
   [NAV.peerLearning]: 'Pair à pair | Wecolearn',
-  [NAV.about]: 'A propos | Wecolearn',
+  [NAV.aboutUs]: 'Qui sommes-nous ? | Wecolearn',
   [NAV.signup]: 'Inscription | Wecolearn',
 };
 
 export const navDescriptions = {
   [NAV.peerLearning]: `Pourquoi apprendre en pair à pair ? `,
-  [NAV.about]: 'Pourquoi Wecolearn ?',
+  [NAV.aboutUs]: 'Qui sommes-nous ?',
   [NAV.signup]: 'Inscription à Wecolearn',
 };
--- front/src/app/modules/doc/doc.module.ts
+++ front/src/app/modules/doc/doc.module.ts
@@ -3,19 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { DocRouting } from '~/modules/doc/doc.routing';
 import { FooterModule } from '~/shared/modules/footer/footer.module';
+import { ContainerComponent } from '~/core/layouts/main/component';
 
-import { ContainerComponent } from '../../core/layouts/main/component';
-import { AboutComponent } from './pages/about/component';
 import { MentionsComponent } from './pages/mentionslegales/component';
 import { PeerLearningComponent } from './pages/peerLearning/component';
 import { CookiePolicyComponent } from './pages/cookie-policy/cookie-policy.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 
 @NgModule({
 
   declarations: [
     ContainerComponent,
-    AboutComponent,
+    AboutUsComponent,
     MentionsComponent,
     PeerLearningComponent,
     CookiePolicyComponent,
