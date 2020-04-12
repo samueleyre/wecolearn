c39e3c7f0ec6746cdb1c1aac30699a39a0312850
Edouard Touraille <edouard.touraille@gmail.com>
2020-04-12 17:28:21 +0200

    co

--- front/src/app/core/layouts/header/component.ts
+++ front/src/app/core/layouts/header/component.ts
@@ -41,6 +41,7 @@ export class HeaderComponent implements OnInit {
 
   ngOnInit() {
     Logged.get().subscribe((logged: boolean) => {
+      console.log('logged');
       this.connected = logged;
       this.clientService.get().subscribe((client: User) => {
         this.currentClient = client;
