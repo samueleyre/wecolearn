61575a144436af3c27ba5c3136d14e12dfe9c44f
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-04-12 17:59:41 +0200

    add: emoji on desktop

--- front/src/app/modules/chat/components/chat-window/chat-window-desktop/chat-window-desktop.component.ts
+++ front/src/app/modules/chat/components/chat-window/chat-window-desktop/chat-window-desktop.component.ts
@@ -1,9 +1,11 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
+import 'quill-emoji/dist/quill-emoji.js';
 
 import { ChatWindowBaseComponent } from '~/modules/chat/components/chat-window/chat-window.base.component';
 import { MessagesService } from '~/modules/chat/services/messages';
 import { Threads } from '~/modules/chat/services/threads';
 import { ClientService } from '~/core/services/user/client';
+
 
 @Component({
   selector: 'app-chat-window-desktop',
@@ -11,6 +13,8 @@ import { ClientService } from '~/core/services/user/client';
   styleUrls: ['./chat-window-desktop.component.scss'],
 })
 export class ChatWindowDesktopComponent extends ChatWindowBaseComponent implements OnInit {
+  public modules = {};
+
   constructor(
     public messagesService: MessagesService,
     public threadsService: Threads,
@@ -18,5 +22,16 @@ export class ChatWindowDesktopComponent extends ChatWindowBaseComponent implemen
     public el: ElementRef,
   ) {
     super(messagesService, threadsService, clientService, el);
+  }
+
+  ngOnInit(): void {
+    super.ngOnInit();
+    this.modules = {
+      'emoji-shortname': true,
+      'emoji-textarea': false,
+      'emoji-toolbar': {
+        buttonIcon: '<mat-icon class="mat-icon notranslate material-icons mat-icon-no-color" role="img" aria-hidden="true">sentiment_satisfied</mat-icon>',
+      },
+    };
   }
 }
