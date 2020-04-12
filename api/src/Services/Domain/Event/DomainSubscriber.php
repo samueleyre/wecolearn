e20bd89c0378564e2f7a530355cb0d0a89dab007
Edouard Touraille <edouard.touraille@gmail.com>
2020-04-12 14:43:46 +0200

    co

--- api/src/Services/Domain/Event/DomainSubscriber.php
+++ api/src/Services/Domain/Event/DomainSubscriber.php
@@ -11,6 +11,8 @@ namespace App\Services\Domain\Event;
 use App\Services\Domain\Service\DomainService;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class DomainSubscriber implements EventSubscriberInterface
@@ -24,11 +26,33 @@ class DomainSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-                KernelEvents::CONTROLLER => 'onKernelController'
+                KernelEvents::REQUEST => ['onKernelRequest', 3000],
+                KernelEvents::CONTROLLER => 'onKernelController',
+                KernelEvents::RESPONSE => 'onKernelResponse',
+
         ];
     }
 
     public function onKernelController(FilterControllerEvent $event ) {
         $this->domainService->setRequest($event->getRequest());
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event ) {
+        $response = $event->getResponse();
+
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,Origin,Accept,Access-Control-Allow-Headers,Access-Control-Allow-Methods,Access-Control-Allow-Origin,accept, x-custom-auth, content-type, authorization, x-pagination');
+        $response->headers->set('Access-Control-Allow-Methods','GET, OPTIONS, POST, PUT, PATCH, DELETE' );
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+
+        $event->setResponse($response);
+    }
+
+    public function onKernelRequest(RequestEvent $event ) {
+        $request = $event->getRequest();
+        foreach($request->headers->all() as $key => $value ) {
+            $chaine = sprintf("%s: %s", $key , $value[0]);
+            syslog ( LOG_ERR, $chaine );
+        }
     }
 }
