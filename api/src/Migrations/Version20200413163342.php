82687c659c596daa7f663b130eff040e0be83dc1
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-04-14 19:26:56 +0200

    update: latest  migration definition

--- api/src/Migrations/Version20200413163342.php
+++ api/src/Migrations/Version20200413163342.php
@@ -19,13 +19,13 @@ final class Version20200413163342 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        $this->addSql('ALTER TABLE `message` CHANGE `message` `message` VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `message` CHANGE `message` `message` VARCHAR(10000) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NULL DEFAULT NULL');
         // this up() migration is auto-generated, please modify it to your needs
     }
 
     public function down(Schema $schema) : void
     {
-        $this->addSql('ALTER TABLE `message` CHANGE `message` `message` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL');
+        $this->addSql('ALTER TABLE `message` CHANGE `message` `message` VARCHAR(10000) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL');
         // this down() migration is auto-generated, please modify it to your needs
     }
 }
