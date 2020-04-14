97092073a98255fdbb44a60e61e65b6353eca94e
Edouard Touraille <edouard.touraille@gmail.com>
2020-04-14 18:11:47 +0200

    accepte l origin flibusteam

--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  origin : 'http://localhost:8080',
+  origin : 'http://flibus.team:8080',
   publique : 'http://localhost:4200',
   mapApiKey :  'AIzaSyAkTAUCcqkD4lbTeQdM5nQIn0Vy5voUQF8',
   production : false,
-  mercureUrl : 'http://localhost:3000/.well-known/mercure',
+  mercureUrl : 'http://flibus.team:3000/.well-known/mercure',
   vapidPublic: 'BI4_eiMKp80OeVlgpIlgz219DCMwQqU1ZDwikXdsOnMBozGUFz0Knrm2503Fp4d9jqGWoMmASuGv6w38ABIc6ZY',
 };
