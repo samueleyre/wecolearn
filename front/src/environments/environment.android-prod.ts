0f2288629fa4d2f41dedf613d3527f49ab4566c2
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-04-28 22:59:08 +0200

    fix: mercurre on android

--- front/src/environments/environment.android-prod.ts
+++ front/src/environments/environment.android-prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   publique : ' https://wecolearn.com',
   mapApiKey : 'AIzaSyA9v3Cne0PPDnAal2Gt1jAbeGmxSym5Ltg',
   production : true,
-  android : false,
+  android : true,
   mercureUrl : 'https://mercure.wecolearn.com/hub',
   vapidPublic : 'BI4_eiMKp80OeVlgpIlgz219DCMwQqU1ZDwikXdsOnMBozGUFz0Knrm2503Fp4d9jqGWoMmASuGv6w38ABIc6ZY',
 };
