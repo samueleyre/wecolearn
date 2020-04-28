c26ba72eb9c09dce7bf634b25e8ef084be67ed6a
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-04-28 22:59:08 +0200

    fix: mercurre on android

--- front/src/environments/environment.capacitor.ts
+++ front/src/environments/environment.capacitor.ts
@@ -1,8 +1,10 @@
+// android dev
 export const environment = {
   origin : 'http://flibus.team:8080',
   publique : 'http://localhost:4200',
   mapApiKey :  'AIzaSyAkTAUCcqkD4lbTeQdM5nQIn0Vy5voUQF8',
   production : false,
+  android : true,
   mercureUrl : 'http://flibus.team:3000/.well-known/mercure',
   vapidPublic: 'BI4_eiMKp80OeVlgpIlgz219DCMwQqU1ZDwikXdsOnMBozGUFz0Knrm2503Fp4d9jqGWoMmASuGv6w38ABIc6ZY',
 };
--- front/src/environments/environment.ts
+++ front/src/environments/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
   publique : 'http://localhost:4200',
   mapApiKey :  'AIzaSyAkTAUCcqkD4lbTeQdM5nQIn0Vy5voUQF8',
   production : false,
+  android : false,
   mercureUrl : 'http://localhost:3000/.well-known/mercure',
   vapidPublic: 'BI4_eiMKp80OeVlgpIlgz219DCMwQqU1ZDwikXdsOnMBozGUFz0Knrm2503Fp4d9jqGWoMmASuGv6w38ABIc6ZY',
 };
