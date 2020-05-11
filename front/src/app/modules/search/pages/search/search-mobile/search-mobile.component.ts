5d156d52dd491f4e572f7579b38a3e7cce5d353c
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-05-11 20:44:45 +0200

    add: first parameter to search to fix scroll search

--- front/src/app/modules/search/pages/search/search-mobile/search-mobile.component.ts
+++ front/src/app/modules/search/pages/search/search-mobile/search-mobile.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { DeviceDetectorService } from 'ngx-device-detector';
 
 import { SearchComponentBase } from '~/modules/search/pages/search/search.component.base';
 import { SearchService } from '~/core/services/search/search';
-import {SEARCH} from "~/modules/search/config/main";
 
 @Component({
   selector: 'app-search-mobile',
@@ -21,7 +19,6 @@ export class SearchMobileComponent extends SearchComponentBase implements OnInit
 
   onScroll(ev) {
     this.timeOutScroll();
-    SearchService.max += SEARCH.scrollLatence;
-    this.search({ max: SearchService.max });
+    super.onScroll(ev);
   }
 }
