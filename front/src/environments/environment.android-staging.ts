4e17e40efe5a982b7651efbfe34e3dda3bc90cdb
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-07-15 12:21:05 +0200

    up: workflow for staging, up: android staging env

--- front/src/environments/environment.android-staging.ts
+++ front/src/environments/environment.android-staging.ts
@@ -1,10 +1,10 @@
-// android dev
+// android staging
 export const environment = {
-  origin : 'http://flibus.team:8080',
-  publique : 'http://localhost:4200',
+  origin : 'https://api-staging.wecolearn.com',
+  publique : 'http://staging.wecolearn.com',
   mapApiKey :  '916b1cc2e5766358f98ec6ee40d66d5f',
-  production : false,
+  production : true,
   android : true,
-  mercureUrl : 'http://flibus.team:3000/.well-known/mercure',
+  mercureUrl : 'https://mercure-staging.wecolearn.com/hub',
   vapidPublic: 'BI4_eiMKp80OeVlgpIlgz219DCMwQqU1ZDwikXdsOnMBozGUFz0Knrm2503Fp4d9jqGWoMmASuGv6w38ABIc6ZY',
 };
