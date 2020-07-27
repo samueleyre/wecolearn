f46e97b3cda4989c17f3e844a85b19feaf2c9980
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-07-27 11:51:10 +0200

    refactor app.module & remove bootstrap from nav menu

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,39 +1,21 @@
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LOCALE_ID, NgModule } from '@angular/core';
 import fr from '@angular/common/locales/fr';
 import { APP_BASE_HREF, registerLocaleData } from '@angular/common';
-import { MatNativeDateModule, MatSliderModule } from '@angular/material';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DeviceDetectorModule } from 'ngx-device-detector';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
-// feature modules
-import { AuthModule } from '~/modules/auth/auth.module';
-import { SearchModule } from '~/modules/search/search.module';
 // shared modules
-import { ProfileImageModule } from '~/modules/profile/modules/profilImage/profileImage.module';
-import { HeaderMobileComponent } from '~/core/layouts/header-mobile/component';
-import { FooterMobileComponent } from '~/core/layouts/footer-mobile/component';
 import { CoreModule } from '~/core/core.module';
-import { SharedModule } from '~/shared/shared.module';
 import { MaterialModule } from '~/shared/modules/material/material.module';
-import { PipesModule } from '~/shared/modules/pipesModule/pipes.module';
 
 // local components
-import { MainComponent } from './core/layouts/dashboard/component';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './pages/notFound/component';
-import { MenuComponent } from './core/layouts/menu/component';
-import { HeaderComponent } from './core/layouts/header/component';
-import { CommunitiesComponent } from './pages/communities/component';
-import { CommunityComponent } from './pages/communities/community/component';
+
 // local modules
 import { WcRoutingModule } from './app-routing.module';
-// shared components
-import { CopyComponent } from './shared/components/copy/component';
-import { AutofocusDirective } from './modules/search/directives/input/directive';
+
 // shared services
 import { HttpApiInterceptor } from './core/services/auth/httpApiInterceptor';
 import { getBaseLocation } from './core/services/layout/baseUrl';
@@ -43,36 +25,14 @@ registerLocaleData(fr);
 @NgModule({
   imports: [
     CoreModule,
-    SharedModule,
-    ProfileImageModule,
-    FormsModule,
     BsDropdownModule.forRoot(),
-    ReactiveFormsModule,
-    MatNativeDateModule,
     WcRoutingModule,
     DeviceDetectorModule.forRoot(),
-    AuthModule,
-    SearchModule,
-    MatSliderModule,
-    MatIconModule,
     MaterialModule,
-    PipesModule,
-    MatToolbarModule,
   ],
   declarations: [
     AppComponent,
     NotFoundComponent,
-    CommunitiesComponent,
-    CommunityComponent,
-    // Directives
-    AutofocusDirective,
-    // Layout
-    MainComponent,
-    MenuComponent,
-    HeaderMobileComponent,
-    HeaderComponent,
-    FooterMobileComponent,
-
   ],
   providers: [
     {
--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -6,15 +6,26 @@ import { ToastrModule } from 'ngx-toastr';
 
 import { CopyComponent } from '~/shared/components/copy/component';
 import { HideMenuMobileOnFocusDirective } from '~/shared/directives/hide-menu-mobile-on-focus.directive';
+import { ProfileImageModule } from '~/modules/profile/modules/profilImage/profileImage.module';
+import { PipesModule } from '~/shared/modules/pipesModule/pipes.module';
 
 @NgModule({
   declarations: [
     CopyComponent,
     HideMenuMobileOnFocusDirective,
   ],
-  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, ToastrModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    ToastrModule,
+    ProfileImageModule,
+    PipesModule,
+  ],
   exports: [
     CommonModule,
+    ProfileImageModule,
     CopyComponent,
     HideMenuMobileOnFocusDirective,
   ],
