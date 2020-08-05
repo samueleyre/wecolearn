2e33aa7d4755df3a13c73b3257a77cc7dce21efe
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-08-05 16:21:17 +0200

    add notification when geolocation is changed, fixed scroll bug

--- front/src/app/modules/search/pages/search/search.component.ts
+++ front/src/app/modules/search/pages/search/search.component.ts
@@ -2,8 +2,6 @@ import {
   Component,
 } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
-import { ToastController } from '@ionic/angular';
-import { from } from 'rxjs';
 
 @Component({
   selector: 'app-search',
@@ -11,15 +9,7 @@ import { from } from 'rxjs';
 })export class SearchComponent {
   constructor(
     private deviceService: DeviceDetectorService,
-    private _toastMobile: ToastController,
   ) {
-    // const toast$ = from(this._toastMobile.create({
-    //   message: 'message',
-    //   duration: 20000,
-    // }));
-    // toast$.subscribe((toast) => {
-    //   toast.present();
-    // });
   }
 
   get isMobile() {
