ec1093b16d350f1fff39284cc13ea8ea5ef66e71
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-08-07 17:18:04 +0200

    fix nav on ionic

---
+++ front/src/app/core/layouts/ionic-container/ionic-container.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ionic-container',
+  templateUrl: './ionic-container.component.html',
+})
+export class IonicContainerComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
