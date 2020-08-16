df80597709a5831891b72d6c0d85855b3618a745
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-08-16 21:20:59 +0200

    redesign searchbar on mobile

--- front/src/app/modules/search/components/searchBar/searchBarDesktop/component.ts
+++ front/src/app/modules/search/components/searchBar/searchBarDesktop/component.ts
@@ -5,8 +5,10 @@ import {
 
 import { TagService } from '~/core/services/tag/tag';
 import { SearchBarBaseComponent } from '~/modules/search/components/searchBar/search-bar.base.component';
+import { Tag } from '~/core/entities/tag/entity';
+import { TagTypeEnum } from '~/core/enums/tag/tag-type.enum';
 
-import { SearchService } from '../../../../../core/services/search/search';
+import { SearchService } from '~/core/services/search/search';
 
 @Component({
   selector: 'app-searchbar',
@@ -18,5 +20,27 @@ import { SearchService } from '../../../../../core/services/search/search';
         private _searchService: SearchService,
     ) {
     super(_tagService, _searchService);
+  }
+
+  search(): void {
+    let tag = null;
+    if (this.searchInputControl) {
+      if (typeof this.searchInputControl.value === 'string') {
+        tag = new Tag({
+          id: null,
+          name: this.searchInputControl.value,
+          type: TagTypeEnum.LEARN,
+        });
+      } else if (this.searchInputControl.value) {
+        tag = this.searchInputControl.value;
+      }
+    }
+    this.searchInputChange.next(this.searchInputControl.value);
+    super.search(tag);
+    this.hideAutocomplete();
+  }
+
+  hideAutocomplete() {
+    this.autocomplete.closePanel();
   }
 }
