d87d484c5a7725614cba844a2035b7fc4a7319cb
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-08-20 15:36:53 +0200

    add: slack link

--- front/src/app/shared/modules/footer/config/socials.ts
+++ front/src/app/shared/modules/footer/config/socials.ts
@@ -4,4 +4,5 @@ export const SOCIALS = {
   telegramLink: 'https://bit.ly/2YcInj9',
   meetupLink : 'https://www.meetup.com/fr-FR/Wecolearn-apprendre-ensemble',
   blogLink : 'https://blog.wecolearn.com',
+  slackLink: 'https://join.slack.com/t/wecolearn/shared_invite/zt-6ln3qq4i-qSWDA2gu1jReRpI6o2hdKg',
 };
