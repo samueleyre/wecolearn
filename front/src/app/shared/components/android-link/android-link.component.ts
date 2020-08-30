b703157f8bd0c519ba5a09c9bc9e54d5ef138697
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-08-30 12:49:16 +0200

    add: link to android on home page

---
+++ front/src/app/shared/components/android-link/android-link.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { LINKS } from '~/config/links';
+
+@Component({
+  selector: 'app-android-link',
+  templateUrl: './android-link.component.html',
+  styleUrls: ['./android-link.component.scss'],
+})
+export class AndroidLinkComponent {
+  public androidLink = LINKS.android;
+}
