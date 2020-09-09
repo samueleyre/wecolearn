09d1b04dd83592641c3a2512464e35dd2994574c
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-09-09 15:16:00 +0200

    more pro description

--- front/src/app/modules/doc/pages/about-us/about-us.component.ts
+++ front/src/app/modules/doc/pages/about-us/about-us.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FOUNDERS_SOCIALS } from '~/modules/doc/config/founders_socials.const';
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
   styleUrls: ['./about-us.component.scss'],
 })
-export class AboutUsComponent implements OnInit {
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class AboutUsComponent {
+  socials = FOUNDERS_SOCIALS;
 }
