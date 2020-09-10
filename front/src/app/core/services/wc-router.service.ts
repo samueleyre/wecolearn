d5bada8dead12193df2293708ec1a20b7a3ed594
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-09-10 18:36:57 +0200

    access profil on admin & chat, fix return link on profil

--- front/src/app/core/services/wc-router.service.ts
+++ front/src/app/core/services/wc-router.service.ts
@@ -1,14 +1,37 @@
 import { Injectable } from '@angular/core';
 import { DeviceDetectorService } from 'ngx-device-detector';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router, RoutesRecognized } from '@angular/router';
+import { filter, pairwise } from 'rxjs/operators';
 
 import { NAV } from '~/config/navigation/nav';
+import { getBaseLocation } from '~/core/services/layout/baseUrl';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WcRouterService {
-  constructor(private _deviceService: DeviceDetectorService, private _router: Router) { }
+  _previousUrl = null;
+
+  constructor(private _deviceService: DeviceDetectorService, private _router: Router) {
+    _router.events
+      .pipe(filter((evt: any) => evt instanceof RoutesRecognized), pairwise())
+      .subscribe((events: RoutesRecognized[]) => {
+        console.log('previous url', events[0].urlAfterRedirects);
+        this._previousUrl = events[0].urlAfterRedirects;
+        console.log('current url', events[1].urlAfterRedirects);
+      });
+  }
+
+  /**
+   * get previous page
+   */
+  public getReturnLink(defaultLink: string) {
+    if (this._previousUrl) {
+      return this._previousUrl;
+    }
+
+    return defaultLink;
+  }
 
   public navigateToCurrentDiscussion(userId, callback): void {
     this._deviceService.isMobile()
