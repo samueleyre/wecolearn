92391856790f7968fe79ba81bb7c2b71d4ef5643
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-12-01 15:36:21 +0100

    remove senty as it makes everything crash

--- front/src/main.ts
+++ front/src/main.ts
@@ -10,20 +10,21 @@ import { EnvEnum } from '~/core/enums/env.enum';
 
 import { environment } from './environments/environment';
 
-if (environment.env === EnvEnum.PRODUCTION || environment.env === EnvEnum.STAGING) {
-  Sentry.init({
-    dsn: 'https://ce81fb50d71e4df6b537456700e5e575@o485043.ingest.sentry.io/5539084',
-    integrations: [
-      new Integrations.BrowserTracing({
-        tracingOrigins: [environment.origin],
-        routingInstrumentation: Sentry.routingInstrumentation,
-      }),
-    ],
+// Sentry.init({
+//   dsn: 'https://ce81fb50d71e4df6b537456700e5e575@o485043.ingest.sentry.io/5539084',
+//   integrations: [
+//     new Integrations.BrowserTracing({
+//       tracingOrigins: [environment.origin],
+//       routingInstrumentation: Sentry.routingInstrumentation,
+//     }),
+//   ],
+//
+//               // We recommend adjusting this value in production, or using tracesSampler
+//               // for finer control
+//   tracesSampleRate: 1.0,
+// });
 
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 1.0,
-  });
+if (environment.env === EnvEnum.PRODUCTION || environment.env === EnvEnum.STAGING) {
   enableProdMode();
 }
 
