96b45851ac5b93f9de188a4807120ca6cec33daa
Samuel Eyre <samuel.eyre@hotmail.fr>
2020-12-08 15:28:10 +0100

    add: mobile specific view of tag selector

--- front/src/app/modules/tags/modules/tag-ui/tag-ui.module.ts
+++ front/src/app/modules/tags/modules/tag-ui/tag-ui.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 import { MaterialModule } from '~/shared/modules/material/material.module';
 import { TagDomainUiModule } from '~/modules/tags/modules/tag-domain-ui/tag-domain-ui.module';
 import { SharedModule } from '~/shared/shared.module';
+import { TagSearchMobileComponent } from '~/modules/tags/modules/tag-ui/components/tag-search-mobile/tag-search-mobile.component';
 
 import { TagFormComponent } from './components/tag-form/tag-form.component';
 import { TagListComponent } from './components/tag-list/tag-list.component';
@@ -12,11 +14,18 @@ import { TagMergeFormComponent } from './components/tag-merge-form/tag-merge-for
 import { TagAutocompleteComponent } from './components/tag-autocomplete/tag-autocomplete.component';
 
 @NgModule({
-  declarations: [TagFormComponent, TagListComponent, TagMergeFormComponent, TagAutocompleteComponent],
+  declarations: [
+    TagFormComponent,
+    TagListComponent,
+    TagMergeFormComponent,
+    TagAutocompleteComponent,
+    TagSearchMobileComponent,
+  ],
   exports: [
     TagListComponent,
     TagFormComponent,
     TagMergeFormComponent,
+    TagSearchMobileComponent,
   ],
   imports: [
     CommonModule,
@@ -24,6 +33,7 @@ import { TagAutocompleteComponent } from './components/tag-autocomplete/tag-auto
     ReactiveFormsModule,
     TagDomainUiModule,
     SharedModule,
+    IonicModule,
   ],
 })
 export class TagUiModule { }
--- front/src/app/modules/tags/tags.module.ts
+++ front/src/app/modules/tags/tags.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ProfileFormModule } from '~/modules/profile/modules/form/form.module';
+import { TagsRoutingModule } from '~/modules/tags/tags-routing.module';
+
+import { ProfileTagsSearchBarPageComponent } from '../tags/pages/profile-tags-search-bar-page/profile-tags-search-bar-page.component';
+import {TagUiModule} from '~/modules/tags/modules/tag-ui/tag-ui.module';
+
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
+  declarations: [
+    ProfileTagsSearchBarPageComponent,
   ],
+            imports: [
+              CommonModule,
+              ProfileFormModule,
+              TagsRoutingModule,
+              TagUiModule,
+            ],
 })
 export class TagsModule { }
