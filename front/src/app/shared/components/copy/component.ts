15d56fd90f8d99414bc4145a30f1a8cd34c4d85b
Samuel Eyre <samuel.eyre@hotmail.fr>
2021-05-31 11:46:36 +0200

    enable copy of link to subscribe to community

--- front/src/app/shared/components/copy/component.ts
+++ front/src/app/shared/components/copy/component.ts
@@ -1,18 +1,23 @@
-import { Component, Injectable, OnInit, Input } from '@angular/core';
+import {Component, Injectable, OnInit, Input, ViewChild, ElementRef} from '@angular/core';
+import {MatTooltip} from '@angular/material';
 
 
 @Component({
   selector: 'copy',
   templateUrl: 'template.html',
+  styleUrls: ['copy.component.scss'],
 })export class CopyComponent implements OnInit {
   @Input() data: string;
   public rand: string;
+
+  @ViewChild('tooltip', { static: false }) tooltip: MatTooltip;
 
   ngOnInit() {
     this.rand = String(Math.floor(Math.random() * (10000))); // tslint:disable-line no-magic-numbers
   }
 
   copy() {
+    this.tooltip.show();
     const input: HTMLInputElement = <HTMLInputElement>document.getElementById('copy' + this.rand);
     input.style.display = 'block';
     input.select();
