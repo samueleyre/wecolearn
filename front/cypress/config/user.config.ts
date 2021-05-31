58f8c122242ef0f11a404a23f4909f891bb8cbf9
Samuel Eyre <samuel.eyre@hotmail.fr>
2021-05-31 13:54:16 +0200

    (cypress) add: test for community link

--- front/cypress/config/user.config.ts
+++ front/cypress/config/user.config.ts
@@ -20,3 +20,13 @@ export const USER_CHANGES_CONFIG: User = <User>{
   city: 'Paris',
   biographie: 'Je veux devenir champion d\'echecs',
 };
+
+export const NEW_COMMUNITY_USER_CONFIG: User = <User>{
+  first_name: 'Jimmy',
+  last_name: 'Daniel',
+  password: 'password',
+  email: 'to be defined',
+  learn_tags: ['polka'],
+  city: 'Lyon',
+  biographie: 'J\'ai envie d\'apprendre la polka avec d\'autres membres de la communauté.',
+};
