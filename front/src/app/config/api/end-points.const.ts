7abaed26b02cdcc3887d17aaa0815dbf21f84198
Samuel Eyre <samuel.eyre@hotmail.fr>
2021-05-31 21:02:09 +0200

    add: test mobile for community link

---
+++ front/src/app/config/api/end-points.const.ts
@@ -0,0 +1,10 @@
+export const ENDPOINTS = {
+  auth: {
+    signIn: '/api/login_check',
+    // signUp: 'api/auth/signup',
+  },
+  communityAdmin : {
+    community: '/api/community-admin/community',
+    generateInviteUrl: '/api/community-admin/generateNewInviteUrl',
+  },
+};
